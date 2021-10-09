15e2f3f3dabc1405a6b122d4951e8b4480ecd0b2
Amaro <ablest@ucm.es>
2021-10-09 21:03:06 +0200

    ADDED: las dos primeras activity

--- app/src/main/java/es/ucm/arblemar/a0hn0/MainActivity.java
+++ app/src/main/java/es/ucm/arblemar/a0hn0/MainActivity.java
@@ -3,6 +3,7 @@ package es.ucm.arblemar.a0hn0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    //
+    public void goToGame(View view){
+        setContentView(R.layout.activity_game_selection);
+
     }
 }
