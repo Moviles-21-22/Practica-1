8cef9d3ad0093cbacdef900ebc05d77965412514
Stiven <starias@ucm.es>
2021-10-19 21:50:23 +0200

    Commit de prueba

--- code/0hn0/DesktopGame/src/main/java/es/ucm/arblemar/desktopgame/Main.java
+++ code/0hn0/DesktopGame/src/main/java/es/ucm/arblemar/desktopgame/Main.java
@@ -1,5 +1,6 @@
 package es.ucm.arblemar.desktopgame;
 import es.ucm.arblemar.gamelogic.TipoCelda;
+
 public class Main {
     public static void main (String [] args){
         TipoCelda t = TipoCelda.AZUL;
--- code/0hn0/GameLogic/src/main/java/es/ucm/arblemar/gamelogic/TipoCelda.java
+++ code/0hn0/GameLogic/src/main/java/es/ucm/arblemar/gamelogic/TipoCelda.java
@@ -1,18 +1,18 @@
 package es.ucm.arblemar.gamelogic;
 
 public enum TipoCelda {
-    GRIS/*(0)*/,
-    AZUL/*(1)*/,
-    ROJO/*(2)*/,
+    GRIS(0),
+    AZUL(1),
+    ROJO(2),
 
-    //MAX/*(3)*/;
+    MAX(3);
 
-    //private int value;
-    //tipoCelda(int i) {
-    //   this.value = i;
-    //
-    //
-    //public int getValue(){
-    //   return value;
-    //
+    private int value;
+    private TipoCelda(int i) {
+        this.value = i;
+    }
+
+    public int getValue(){
+       return value;
+    }
 };
